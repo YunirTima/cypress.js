e0e40af33a5f564d907cfbe1fb9cb64685201b2c
YunirTima <yunir797@mail.ru>
2024-09-09 16:16:08 +0500

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -0,0 +1,60 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный логин и верный пароль', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#forgotForm > .header').contains('Восстановите пароль');
+        cy.get('#forgotForm').should('be.visible');
+        cy.get('#exitRestoreButton > .exitIcon').should('be.visible');
+    })
+
+    it('Верный логин и Неверный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('ILoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('Kerman@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Логин без @ и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Верный логин в строчные буквы и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+ })
